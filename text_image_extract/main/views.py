f157de999fafe412388f1bf13c59e7f3e75d5c9b
frivo <friv14666@gmail.com>
2023-10-28 13:54:33 +0100

    modify the text display and add grammar expressions before sending the json data

--- text_image_extract/main/views.py
+++ text_image_extract/main/views.py
@@ -16,6 +16,10 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
+import json
+import pytesseract
+import re
+
 def upload_image(request):
     if request.method == 'POST':
         uploaded_image = request.FILES.get('image')
@@ -29,24 +33,41 @@ def upload_image(request):
             # Perform text extraction here
             extracted_text = extract_text(image_path)
             if extracted_text:
-                # Create a new ExtractedText object and save it
-                extracted_text_obj = ExtractedText(image=uploaded_image, extracted_text=extracted_text)
-                extracted_text_obj.save()
+                # Clean and format the extracted text
+                cleaned_text = clean_and_format_text(extracted_text)
 
-                return JsonResponse({'status': 'success', 'text': extracted_text})
+                # Create a dictionary to structure the JSON response
+                response_data = {
+                    'status': 'success',
+                    'text': cleaned_text
+                }
+                print(cleaned_text)
+                print(json.dumps(response_data, indent=2))
+                return JsonResponse(response_data)
 
     return JsonResponse({'status': 'error', 'message': 'Image upload or text extraction failed.'})
 
-
-
-
-def extract_text(image):
+def extract_text(image_path):
     try:
-        img = Image.open(image)
+        img = Image.open(image_path)
         extracted_text = pytesseract.image_to_string(img, config=settings.TESSERACT_CMD)
         return extracted_text
     except Exception as e:
         return str(e)
+
+
+
+def clean_and_format_text(text):
+    # Replace multiple line breaks with a single line break
+    cleaned_text = re.sub(r'\n+', '\n', text)
+
+    # Replace multiple spaces with a single space
+    cleaned_text = re.sub(r' +', ' ', cleaned_text)
+
+    # Maintain code blocks in their structure
+    cleaned_text = re.sub(r'```(.*?)```', r'```\1```', cleaned_text, flags=re.DOTALL)
+    
+    return cleaned_text
 
 
 
